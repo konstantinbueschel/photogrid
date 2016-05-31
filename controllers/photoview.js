58da2a64a27dbdb46bff039de1bd1583dd61e073
Konstantin Büschel <konstantin@die-interaktiven.de>
2016-05-31 17:59:40 +0200

    * changed formatting * updated view structure * added Titanium default fullscreen no title bar theme * added some cleanup handling * removed unnecessary code lines and debug logs

--- controllers/photoview.js
+++ controllers/photoview.js
@@ -1,45 +1,125 @@
-var args = arguments[0] || {},
-    data = [];
+var LTAG = '[PhotoGridWidget]';
 
+
+/**
+ * self-executing function to organize otherwise inline constructor code
+ *
+ * @param  {Object} args arguments passed to the controller
+ * @returns void
+ */
+(function constructor(args) {
+
+	// variable declaration
+	$.data       = [];
+	$.intervalId = null;
 
 if (_.has(args, 'data')){
     _.each(args.data, function(photo){
         var photoView = Ti.UI.createImageView({
             width: Ti.UI.FILL,
             height: Ti.UI.SIZE,
-            image: photo.image
+
+				image:        photo.image,
+				defaultImage: WPATH('/images/no_image.jpg')
         });
-        data.push(photoView);
+
+			$.data[$.data.length] = photoView;
     });
     
-    $.photos.setViews(data);
+		$.photos.setViews($.data);
     
     _.has(args, 'index') && $.photos.setCurrentPage(args.index);
 }
 
+
+	if (_.has(args, 'interval') && args.interval !== 0) {
+
+		$.intervalId = setInterval(function () {
+
+			autoScroll();
+
+		}, args.interval);
+	}
+
+
+	OS_IOS && $.photos_win.setTransform(Ti.UI.create2DMatrix().scale(0, 0));
+
+// execute constructor with optional arguments passed to controller
+})($.args);
+
+
 function autoScroll() {
-    var intCurrentIndex = $.photos.getCurrentPage();
-    var intTotalViews = $.photos.getViews().length;
-    Ti.API.debug("$.photos: " + JSON.stringify($.photos));
-    Ti.API.debug("currentIndex: " + intCurrentIndex);
+
+	var intCurrentIndex = $.photos.getCurrentPage(),
+		intTotalViews   = $.photos.getViews().length;
+
+
     if (intCurrentIndex === (intTotalViews - 1)) {
         intCurrentIndex = 0;
     } else {
         intCurrentIndex++;
     }
     $.photos.setCurrentPage(intCurrentIndex);
-}
 
-if (_.has(args, 'interval') && args.interval !== 0) {
-    var intervalId = setInterval(function () {
-        autoScroll();
-    }, args.interval);
-}
+} // END autoScroll()
 
 
-$.photos.addEventListener('click', function(){
+function init() {
+
+	$.photos_win.animate({
+
+		opacity:   1.0,
+		transform: Ti.UI.create2DMatrix(),
+		duration:  250
+	});
+
+	return;
+
+} // END init()
+
+
+function cleanup() {
+
+Ti.API.debug(LTAG, 'Cleaning up...');
+
+	$.off();
+	$.destroy();
+	$.removeListener();
+
+	$.intervalId && clearInterval($.intervalId);
+
+	return;
+
+} // END cleanup()
+
+
+function onPhotoClick(event) {
+
+	if (OS_ANDROID) {
+
     $.photos_win.close();
-    if (_.has(args, 'interval')) {
-        clearInterval(intervalId);
     }
+	else {
+
+		$.photos_win.animate({
+
+			opacity:   0.0,
+			transform: Ti.UI.create2DMatrix().scale(0, 0),
+			duration:  300
+
+		}, function () {
+
+			$.photos_win.close();
 });
+
+
+		_.delay(function() {
+
+			$.photos_win.setBackgroundColor('transparent');
+
+		}, 100);
+	}
+
+	return;
+
+} // END onPhotoClick()
--- controllers/widget.js
+++ controllers/widget.js
@@ -1,245 +1,296 @@
-var args = arguments[0] || {},
+var LTAG = '[PhotoGridWidget]',
 
-    // default grid configuration
-    defaults = {
-        // column count in portrait mode
-        portraitColumns: 3,
-        // column count in landscape mode
-        landscapeColumns: 5,
-        // space between thumbs
-        space: 0,
-        // wether title should show up on thumbs or not
-        showTitle: false,
-        //scroll interval to automatically scroll through the photos
-        interval: 0
-    },
+	args = $.args,
 
-    // grid data
-    data = [],
-    
-    // passed args + defaults
-    options = _.defaults(args, defaults);
+// default grid configuration
+	defaults = {
+		// column count in portrait mode
+		portraitColumns: 3,
+		// column count in landscape mode
+		landscapeColumns: 5,
+		// space between thumbs
+		space: 0,
+		// wether title should show up on thumbs or not
+		showTitle: false,
+		//scroll interval to automatically scroll through the photos
+		interval: 0
+	},
+
+// grid data
+	data = [],
+
+// passed args + defaults
+	options = _.defaults(args, defaults);
 
 // apply properties of Ti.UI.View that can be applied to paging control view
 [
-    "backgroundColor",
-    "backgroundImage",
-    "backgroundLeftCap",
-    "backgroundRepeat",
-    "backgroundTopCap",
-    "borderRadius",
-    "borderWidth",
-    "bottom",
-    "height",
-    "horizontalWrap",
-    "left",
-    "opacity",
-    "right",
-    "top",
-    "visible",
-    "width",
-    "zIndex"
-].forEach(function(prop){ _.has(args, prop) && ($.gridView[prop] = args[prop]); });
+	"backgroundColor",
+	"backgroundImage",
+	"backgroundLeftCap",
+	"backgroundRepeat",
+	"backgroundTopCap",
+	"borderRadius",
+	"borderWidth",
+	"bottom",
+	"height",
+	"horizontalWrap",
+	"left",
+	"opacity",
+	"right",
+	"top",
+	"visible",
+	"width",
+	"zIndex"
+].forEach(function (prop) {
+	_.has(args, prop) && ($.gridView[prop] = args[prop]);
+});
 
-if (_.has(args, 'data')){
-    data = args.data;
-    setData(data);
+if (_.has(args, 'data')) {
+	data = args.data;
+	setData(data);
 }
 
 /**
  * set items to the grid
  * @param {Array} List of items (item is an {Object} containing image, thumb and title)
  */
-function setData(_data){
-    
-    data = _data;
-    clearGrid();
-    
-    var thumbSize = getThumbSize();
-    
-    for (var i = 0; i < data.length; i++){
-        addItem(data[i], i, thumbSize);
-        
-    }
-};
+function setData(_data) {
+
+	data = _data;
+	clearGrid();
+
+	var thumbSize = getThumbSize();
+
+	data.forEach(function (dataItem, index) {
+
+		addItem(dataItem, index, thumbSize);
+	});
+
+} // END setData()
+
 
 /**
  * adds a single item to the grid
  */
-function addItem(item, _index, _thumbSize){
-    var index = _index || data.length,
-        thumbImage = item.thumb || item.image,
-        thumbSize = _thumbSize || getThumbSize();
-    
-    if ('undefined' === typeof _index){
-        data.push(item);
-    }
-    
-    var itemViewOpts = {
-        width: thumbSize,
-        height: thumbSize,
-        top: options.space,
-        left: options.space,
-        _image: item.image,
-        _index: index
-    };
-    // iOS seems to not support remote background Images on Views, so change it to ImageView
-    // Tested in iOS and Android, working
-    var itemView = (
-        typeof thumbImage !== "string" && 
-        _.has(thumbImage, 'apiName') && 
-        thumbImage.apiName === "Ti.UI.View"
-    ) ? thumbImage : Ti.UI.createImageView({image: thumbImage});
-    
-    itemView.applyProperties(itemViewOpts);
-    
-    // BUG: cannot apply not legacy properties with applyProperties, so do it now manually or index will be lost
-    // Ti.API.info('final Item: ' + JSON.stringify(itemView));
-    itemView._image = item.image;
+function addItem(item, _index, _thumbSize) {
+	var index = _index || data.length,
+		thumbImage = item.thumb || item.image,
+		thumbSize = _thumbSize || getThumbSize();
+
+	if ('undefined' === typeof _index) {
+
+		data[data.length] = item;
+	}
+
+	var itemViewOpts = {
+		width: thumbSize,
+		height: thumbSize,
+		top: options.space,
+		left: options.space,
+		_image: item.image,
+		_index: index
+	};
+	// iOS seems to not support remote background Images on Views, so change it to ImageView
+	// Tested in iOS and Android, working
+	var itemView = (
+		typeof thumbImage !== "string" &&
+		_.has(thumbImage, 'apiName') &&
+		thumbImage.apiName === "Ti.UI.View"
+	) ? thumbImage : Ti.UI.createImageView({
+
+		image: thumbImage,
+		preventDefaultImage: true
+	});
+
+	itemView.applyProperties(itemViewOpts);
+
+	// BUG: cannot apply not legacy properties with applyProperties, so do it now manually or index will be lost
+	itemView._image = item.image;
 	itemView._index = index;
 
-    if (options.showTitle){
-        
-        var titleView = Ti.UI.createView({
-            width: Ti.UI.FILL,
-            height: thumbSize * 0.2,
-            backgroundColor: '#000',
-            opacity: 0.7,
-            bottom: 0
-        });
-        
-        var titleLabel = Ti.UI.createLabel({
-            text: item.title,
-            width: Ti.UI.FILL,
-            height: (thumbSize * 0.2) - 6,
-            left: 4,
-            font: {
-                fontSize: 14
-            },
-            ellipsize: true,
-            color: '#fff',
-            textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT
-        });
-        
-        titleView.add(titleLabel);
-        itemView.add(titleView);
-    }
-    
-    itemView.addEventListener('click', item.type === 'button' ? item.callback : onItemSelected);
-    $.gridView.add(itemView);
-    
-    return index;
-};
+	if (options.showTitle) {
 
-function removeItem(_index){
-  
-  var itemView = $.gridView.getChildren()[_index];
-  
-  if (!!itemView){
-    itemView.removeEventListener('click', onItemSelected);
-    $.gridView.remove(itemView);
-    itemView = null;
-    
-    console.log('photogrid: item removed');
-  }else{
-    console.log('photogrid: item does not exist');
-  }
-}
+		var titleView = Ti.UI.createView({
+			width: Ti.UI.FILL,
+			height: thumbSize * 0.2,
+			backgroundColor: '#000',
+			opacity: 0.7,
+			bottom: 0
+		});
+
+		var titleLabel = Ti.UI.createLabel({
+			text: item.title,
+			width: Ti.UI.FILL,
+			height: (thumbSize * 0.2) - 6,
+			left: 4,
+			font: {
+				fontSize: 14
+			},
+			ellipsize: true,
+			color: '#fff',
+			textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT
+		});
+
+		titleView.add(titleLabel);
+		itemView.add(titleView);
+	}
+
+	$.addListener(itemView, 'click', item.type === 'button' ? item.callback : onItemSelected);
+
+	$.gridViewContainer.add(itemView);
+
+	return index;
+
+} // END addItem()
+
+
+function removeItem(_index) {
+
+	var itemView = $.gridViewContainer.getChildren()[_index];
+
+	if (!!itemView) {
+
+		$.removeListener(itemView, 'click', onItemSelected);
+
+		$.gridViewContainer.remove(itemView);
+
+		itemView = null;
+
+		Ti.API.log(LTAG, 'Item removed');
+	}
+	else {
+
+		Ti.API.log(LTAG, 'Item does not exist');
+	}
+
+} // END removeItem()
+
 
 /**
  * removes data from grid
  */
-function clearGrid(){
-    
-    if ($.gridView.children.length > 0){
-        
-        _.each($.gridView.getChildren, function(itemView){
-            itemView.removeEventListener('click', onItemSelected);
-            $.gridView.remove(itemView);
-            itemView = null;
-        });
-        
-        $.gridView.removeAllChildren();
-    }
-};
+function clearGrid() {
+
+	if ($.gridViewContainer.children.length > 0) {
+
+		_.each($.gridViewContainer.getChildren, function (itemView) {
+
+			$.removeListener(itemView, 'click', onItemSelected);
+
+			$.gridViewContainer.remove(itemView);
+
+			itemView = null;
+		});
+
+		$.gridViewContainer.removeAllChildren();
+	}
+
+} // END clearGrid()
+
 
 /**
  * removes last item from grid
  */
-function removeLastItem(){
-    data.splice(data.length -1, 1);
-    var itemView = _.last($.gridView.getChildren());
-    itemView.removeEventListener('click', onItemSelected);
-    $.gridView.remove(itemView);
-    itemView = null;
-};
+function removeLastItem() {
+	data.splice(data.length - 1, 1);
+
+	var itemView = _.last($.gridViewContainer.getChildren());
+
+	$.removeListener(itemView, 'click', onItemSelected);
+
+	$.gridViewContainer.remove(itemView);
+
+	itemView = null;
+
+} // END removeLastItem()
+
 
 /**
  * calculate thumb size
- * @return {Number} width / height in dp 
+ * @return {Number} width / height in dp
  */
-function getThumbSize(){
-    
-    var orientation = Ti.Gesture.orientation,
-        screenWidth = Ti.Platform.displayCaps.getPlatformWidth(),
-        thumbSize,
-        columns = 0;
+function getThumbSize() {
 
-    OS_ANDROID && (screenWidth /= Ti.Platform.displayCaps.logicalDensityFactor);
-    
-    if (orientation == Ti.UI.LANDSCAPE_LEFT || orientation == Ti.UI.LANDSCAPE_RIGHT){
-        columns = options.landscapeColumns;
-    }else{
-        columns = options.portraitColumns;
-    }
-    
-    thumbSize = (screenWidth - ( (columns+1) * options.space )) / columns;
-    return Math.floor(thumbSize);
-};
+	var orientation = Ti.Gesture.orientation,
+		screenWidth = Ti.Platform.displayCaps.getPlatformWidth(),
+		thumbSize,
+		columns = 0;
+
+	OS_ANDROID && (screenWidth /= Ti.Platform.displayCaps.logicalDensityFactor);
+
+	if (orientation == Ti.UI.LANDSCAPE_LEFT || orientation == Ti.UI.LANDSCAPE_RIGHT) {
+		columns = options.landscapeColumns;
+	}
+	else {
+		columns = options.portraitColumns;
+	}
+
+	thumbSize = (screenWidth - ( (columns + 1) * options.space )) / columns;
+	return Math.floor(thumbSize);
+
+} // END getThumbSize()
+
 
 /**
  * thumbnail click-listener callback
  * @param {Object} e
  */
-function onItemSelected(e){
-    Ti.API.info('onItemSelected: ' + e.source);
-    
-    var detailWindow = Widget.createController('photoview', {
-        data: data,
-        index: e.source._index,
-        interval: options.interval
-    });
-    
-    detailWindow.getView().open();
-};
+function onItemSelected(e) {
+
+	var detailWindow = Widget.createController('photoview', {
+		data: data,
+		index: e.source._index,
+		interval: options.interval
+	});
+
+	detailWindow.getView().open();
+
+} // END onItemSelected()
+
 
 /**
  * resize thumbnails on orientation change
  * @param {Object} e
  */
-function onOrientationChange(e){
-    
-    var newSize = getThumbSize();
-    
-    _.each($.gridView.getChildren(), function(itemView){
-        itemView.setWidth(newSize);
-        itemView.setHeight(newSize);
-    });
-};
+function onOrientationChange(e) {
+
+Ti.API.debug(LTAG, 'Orientation changed');
+
+	var newSize = getThumbSize();
+
+	_.each($.gridViewContainer.getChildren(), function (itemView) {
+
+		itemView.setWidth(newSize);
+		itemView.setHeight(newSize);
+	});
+
+} // END onOrientationChange()
+
 
 /**
  * remove EventListener
  */
-function cleanUp(){
-  Ti.Gesture.removeEventListener('orientationchange', onOrientationChange);
-}
+function cleanUp() {
+
+Ti.API.debug(LTAG, 'Cleaning up...');
+
+	$.off();
+	$.destroy();
+	$.removeListener();
+
+	Ti.Gesture.removeEventListener('orientationchange', onOrientationChange);
+
+} // END cleanUp()
+
 
 /**
  * initialzation
  */
-function init(){
-  Ti.Gesture.addEventListener('orientationchange', onOrientationChange);
-}
+function init() {
+	Ti.Gesture.addEventListener('orientationchange', onOrientationChange);
+
+} // END init()
+
 
 // grid API
 exports.setData = setData;
